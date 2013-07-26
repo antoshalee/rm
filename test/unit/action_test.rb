36538bab128f8d7e20bc635de9aadb9efd2a12fc
antoshalee <antoshalee@gmail.com>
2013-07-26 13:25:00 +0800

    deleted files

--- test/unit/action_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ActionTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Action.new.valid?
-  end
-end
--- test/unit/helpers/wholesale_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class WholesaleHelperTest < ActionView::TestCase
-end
