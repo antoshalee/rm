ee3201dc02286459637a00a884d56280b267ba2a
antoshalee <antoshalee@gmail.com>
2013-07-30 00:24:19 +0800

    offers slider

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -2,5 +2,6 @@ class StartController < ApplicationController
   def index
     @last_articles = Article.order('created_at desc').limit(3)
     @banners = Banner.order('position')
+    @offers = Offer.limit(3)
   end
 end
