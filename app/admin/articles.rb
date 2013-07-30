5a9397b5abaa029bd603bc9fe51a5cea0457e854
antoshalee <antoshalee@gmail.com>
2013-07-30 12:02:09 +0800

    improved admin

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -4,12 +4,26 @@ ActiveAdmin.register Article do
   form :html => { :enctype=> 'multipart/form-data'} do |f|
     f.inputs do
       f.input :title
-      f.input :lead
+      f.input :lead, :input_html => {rows: 4}
       f.input :content, as: :ckeditor
       f.input :image
     end
 
     f.buttons
+  end
+
+  show do
+    attributes_table do
+      row :title
+      row :image do |article|
+        image_tag article.image.url, width: 100
+      end
+      row :lead
+      row :content do |article|
+        raw article.content
+      end
+      row :created_at
+    end
   end
 
 end
