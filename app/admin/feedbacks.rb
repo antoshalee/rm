9b6da5a0e6e220e1a3ad75636d4674dd8d1e6364
antoshalee <antoshalee@gmail.com>
2013-07-30 12:25:57 +0800

    localize feedbacks

--- app/admin/feedbacks.rb
+++ app/admin/feedbacks.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register Feedback do
   config.clear_sidebar_sections!
+  actions :all, :except => [:new, :edit]
+
+  index do
+    column :name
+    column :email
+    column :text
+    column :created_at
+    default_actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :email
+      row :text
+      row :created_at
+    end
+  end
 end
