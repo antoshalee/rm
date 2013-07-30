0e49322e3d69c4f4da01e6b41732914aea01d435
antoshalee <antoshalee@gmail.com>
2013-07-30 13:13:57 +0800

    precompile path

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,9 @@ module Remix
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w[active_admin.js
+        admin_frontend.js
+        remix-ie.css
+        ckeditor/plugins/mediaembed/plugin.js]
   end
 end
