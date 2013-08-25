4896c78b8b7dc149809b85f156c5984657dad1e8
antoshalee <antoshalee@gmail.com>
2013-08-25 19:02:04 +0800

    localize subscr. type, added subscr. filter, fix correctness of setting of right type value

--- app/admin/subscriptions.rb
+++ app/admin/subscriptions.rb
@@ -1,5 +1,16 @@
 # encoding: utf-8
 ActiveAdmin.register Subscription do
-  config.clear_sidebar_sections!
   menu parent: "Журналы"
+  filter :kind, :as => :check_boxes, :collection => [['Бумажная', "paper"],['Электронная', "digital"]]
+
+  index do
+    column :name
+    column :kind do |s|
+      I18n.t("subscriptions.#{s.kind}")
+    end
+    column :email
+    column :phone
+    column :address
+    column :created_at
+  end
 end
