693a8caf0345f65b9846e7cab9d47a0e2706630a
antoshalee <antoshalee@gmail.com>
2013-09-06 13:43:35 +0800

    Add fullscreen version for album image uploader

--- app/uploaders/album_item_image_uploader.rb
+++ app/uploaders/album_item_image_uploader.rb
@@ -4,4 +4,8 @@ class AlbumItemImageUploader < BaseImageUploader
   version :thumb do
     process :resize_to_fill => [180, 120]
   end
+
+  version :fullscreen do
+    process :resize_to_limit => [1024, 1024]
+  end
 end
