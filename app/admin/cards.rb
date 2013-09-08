dd7407a954bfdcecbcfe08ee01a7b24a01e6e198
antoshalee <antoshalee@gmail.com>
2013-09-08 23:09:51 +0800

    improve cards

--- app/admin/cards.rb
+++ app/admin/cards.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Card do
-  
+  filter :number
+  filter :discount
+  actions :all, except: [:show]
+
+  index do
+    selectable_column
+    column :number
+    column :discount
+    default_actions
+  end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,7 @@
 class Card < ActiveRecord::Base
   attr_accessible :discount, :number
+
+  def to_s
+    number
+  end
 end
