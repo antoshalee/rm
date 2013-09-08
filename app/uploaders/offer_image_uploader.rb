dddc1007d7d2f9b32402126c7c99835b244e8109
antoshalee <antoshalee@gmail.com>
2013-09-08 20:47:55 +0800

    offer uploader version

--- app/uploaders/offer_image_uploader.rb
+++ app/uploaders/offer_image_uploader.rb
@@ -4,4 +4,8 @@ class OfferImageUploader < BaseImageUploader
   version :aside do
     process resize_to_fill: [190,160]
   end
+
+  version :main do
+    process resize_to_limit: [512, 512]
+  end
 end
