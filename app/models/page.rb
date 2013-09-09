7a841e7b6a50f1017aca6fb8514bc7af20055b55
antoshalee <antoshalee@gmail.com>
2013-09-09 10:09:56 +0800

    validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,6 @@ class Page < ActiveRecord::Base
   attr_accessible :content, :title, :url, :sidebar_items_attributes
   has_many :sidebar_items, dependent: :destroy, order: :position
   accepts_nested_attributes_for :sidebar_items, allow_destroy: true
+  validates :title, presence: true
+  validates :url, presence: true
 end
