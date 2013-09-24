b8fce57be3fbc7a79bc300a360e308849115a1c0
antoshalee <antoshalee@gmail.com>
2013-09-24 14:27:05 +0800

    don't show discount offers in slider

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -2,7 +2,7 @@ class StartController < ApplicationController
   def index
     @last_articles = Article.order('created_at desc').limit(3)
     @banners = Banner.order('position')
-    @offers = Offer.limit(3)
+    @offers = Offer.where(discount: false).limit(3)
     @last_album = Album.order('id').last
     @last_magazine = Magazine.order('position desc').first
   end
