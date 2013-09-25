49f8f4fbce874bfa45d36f46e8fa84f5e0569c53
antoshalee <antoshalee@gmail.com>
2013-09-25 09:26:38 +0800

    removve simple auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter :authenticate
-
-  protected
-
-  def authenticate
-    if Rails.env.production?
-      authenticate_or_request_with_http_basic do |username, password|
-        username == "remix" && password == "remixhtvbrc"
-      end
-    end
-  end
 end
