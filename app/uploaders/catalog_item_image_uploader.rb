af7dae2ebec44145075397d6349743ff3afc915a
antoshalee <antoshalee@gmail.com>
2013-09-25 13:24:35 +0800

    change filename for catalog items' images

--- app/uploaders/catalog_item_image_uploader.rb
+++ app/uploaders/catalog_item_image_uploader.rb
@@ -3,4 +3,8 @@ class CatalogItemImageUploader < BaseImageUploader
   version :thumb do
     process resize_to_limit: [450, 450]
   end
+
+  def filename
+    "#{model.id}.#{file.extension}"
+  end
 end
