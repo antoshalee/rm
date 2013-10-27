c1c23fe4d07d7321c832e0d49a656641ad76f42b
Anton Lee <antoshalee@gmail.com>
2013-10-27 17:13:31 +0400

    display random photos

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -3,7 +3,7 @@ class StartController < ApplicationController
     @last_articles = Article.order('created_at desc').limit(3)
     @banners = Banner.order('position')
     @offers = Offer.where(discount: false).order("position asc").limit(3)
-    @last_album = Album.order('id').last
+    @album_items = AlbumItem.random
     @last_magazine = Magazine.order('position asc').first
   end
 end
--- app/models/album_item.rb
+++ app/models/album_item.rb
@@ -2,4 +2,8 @@ class AlbumItem < ActiveRecord::Base
   attr_accessible :image, :description
   belongs_to :album
   mount_uploader :image, AlbumItemImageUploader
+
+  def self.random
+    order "RANDOM()"
+  end
 end
