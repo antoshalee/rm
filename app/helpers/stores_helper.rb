9ee214aa3fb405a72a93996b2b3902372201c593
Anton Lee <antoshalee@gmail.com>
2013-12-22 23:53:33 +0400

    set links to google map

--- app/helpers/stores_helper.rb
+++ app/helpers/stores_helper.rb
@@ -2,4 +2,8 @@ module StoresHelper
   def store_info(s)
   	[s.address, s.phone, s.opening_hours].select { |x| x.present? }.join ", "
   end
+
+  def url_to_store_location_on_gmap store
+  	"https://maps.google.com/?hl=ru&q=loc:#{store.lat},#{store.lng}"
+  end
 end
